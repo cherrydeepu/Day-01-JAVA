b40e30c255fceeb79e8689f1aa9a0c6a5631c926
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-02 21:03:42 +0530

    Add files via upload

---
+++ Stringformats.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Stringformats{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        String a=S.next();
+        System.out.println("Congratulations" + a + "you won 5 lakhs");
+    }
+}
