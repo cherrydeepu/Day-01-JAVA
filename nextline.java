9ac49a6e654c5a0e8b6976094d855d3560aecbbd
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-02 15:53:29 +0530

    Add files via upload

---
+++ nextline.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class nextline{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
